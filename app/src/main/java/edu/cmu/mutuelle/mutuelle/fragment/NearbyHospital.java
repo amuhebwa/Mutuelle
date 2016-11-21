ab08a0839ec4eb8fe5782f81ebc095f77b74a9f7
amuhebwa <muhebwa.grey@gmail.com>
2016-11-21 14:58:42 +0200

    First commit

---
+++ app/src/main/java/edu/cmu/mutuelle/mutuelle/fragment/NearbyHospital.java
@@ -0,0 +1,30 @@
+package edu.cmu.mutuelle.mutuelle.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.cmu.mutuelle.mutuelle.R;
+
+
+public class NearbyHospital extends Fragment
+{
+
+
+    public NearbyHospital()
+    {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.nearbyhospital_fragment, container, false);
+    }
+
+}
---
+++ app/src/main/java/edu/cmu/mutuelle/mutuelle/fragment/Subscription.java
@@ -0,0 +1,28 @@
+package edu.cmu.mutuelle.mutuelle.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.cmu.mutuelle.mutuelle.R;
+
+public class Subscription extends Fragment
+{
+
+
+    public Subscription()
+    {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.subscription_fragment, container, false);
+    }
+
+}
