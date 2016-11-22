507be1b01b328f41a0b70d905dcabe0aced5a63d
amuhebwa <muhebwa.grey@gmail.com>
2016-11-22 07:05:09 +0200

    final changes to the login ui

--- app/src/main/java/edu/cmu/mutuelle/mutuelle/LoginActivity.java
+++ app/src/main/java/edu/cmu/mutuelle/mutuelle/LoginActivity.java
@@ -1,7 +1,10 @@
 package edu.cmu.mutuelle.mutuelle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_login);
+        Button signUpButton = (Button) findViewById(R.id.signupButton);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+            }
+        });
     }
 }
