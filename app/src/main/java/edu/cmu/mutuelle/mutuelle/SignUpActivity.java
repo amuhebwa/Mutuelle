24965824abfaee98484ce3880735f84757797828
amuhebwa <muhebwa.grey@gmail.com>
2016-11-23 16:28:34 +0200

    Added xml elements for signup page

--- app/src/main/java/edu/cmu/mutuelle/mutuelle/SignUpActivity.java
+++ app/src/main/java/edu/cmu/mutuelle/mutuelle/SignUpActivity.java
@@ -1,13 +1,25 @@
 package edu.cmu.mutuelle.mutuelle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class SignUpActivity extends AppCompatActivity {
+    private EditText firstName, lastName, password, nationalID;
+    private ImageButton takePicture;
+    private Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_activity);
+        firstName = (EditText) findViewById(R.id.firstname);
+        lastName = (EditText) findViewById(R.id.lastname);
+        password = (EditText) findViewById(R.id.password);
+        nationalID = (EditText) findViewById(R.id.nationalId);
+        takePicture = (ImageButton) findViewById(R.id.takePicture);
+        signupButton = (Button) findViewById(R.id.signupButton);
     }
 }
