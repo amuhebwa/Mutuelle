ed391328b359e6fd2a70371d5be42575e3df9c43
amuhebwa <muhebwa.grey@gmail.com>
2016-11-23 18:24:01 +0200

    added model classes for user, owner and dependant

--- app/src/main/java/edu/cmu/mutuelle/mutuelle/models/Dependant.java
+++ app/src/main/java/edu/cmu/mutuelle/mutuelle/models/Dependant.java
@@ -1,5 +1,37 @@
 package edu.cmu.mutuelle.mutuelle.models;
 
 
-public class Dependant {
+public class Dependant
+{
+    private int dependantKey;
+    private int dependentId;
+
+    public Dependant()
+    {
+
+    }
+
+    public int getDependantKey()
+    {
+        return dependantKey;
+    }
+
+    public void setDependantKey(int newDependantKey)
+    {
+        dependantKey = newDependantKey;
+    }
+
+    public int getDependentId()
+    {
+        return dependentId;
+    }
+
+    public void setDependentId(int newDependentId)
+    {
+        dependentId = newDependentId;
+    }
+    public String toString()
+    {
+        return String.format("%s, %s", dependentId, dependantKey);
+    }
 }
--- app/src/main/java/edu/cmu/mutuelle/mutuelle/models/Owner.java
+++ app/src/main/java/edu/cmu/mutuelle/mutuelle/models/Owner.java
@@ -1,5 +1,49 @@
 package edu.cmu.mutuelle.mutuelle.models;
 
-public class Owner extends User{
+import java.util.ArrayList;
 
+public class Owner extends User{
+    private String userName;
+    private String password;
+    private ArrayList<Dependant> dependants;
+    public Owner(String newUserName, String newPassword)
+    {
+        userName = newUserName;
+        password = newPassword;
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public void setUserName(String newUserName)
+    {
+        userName = newUserName;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String newPassword)
+    {
+        password = newPassword;
+    }
+
+    public ArrayList<Dependant> getDependants()
+    {
+        return dependants;
+    }
+
+    public void setDependants(ArrayList<Dependant> newDependants)
+    {
+        dependants = newDependants;
+    }
+
+    public String toString()
+    {
+        return String.format("%s %s", userName, password);
+    }
 }
