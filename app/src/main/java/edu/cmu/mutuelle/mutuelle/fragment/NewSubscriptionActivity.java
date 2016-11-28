cab4cd6d6af0b9809c7c46c06be519b7b0410b57
romalice1 <rommy_91@hotmail.fr>
2016-11-28 16:43:06 +0200

    Aded subscription, payment and nearby activities and views

---
+++ app/src/main/java/edu/cmu/mutuelle/mutuelle/fragment/NewSubscriptionActivity.java
@@ -0,0 +1,13 @@
+package edu.cmu.mutuelle.mutuelle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewSubscriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_subscription);
+    }
+}
