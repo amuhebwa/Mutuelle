e77bb5da8c271e0837e3c1e8e13447de13e561b7
romalice1 <rommy_91@hotmail.fr>
2016-11-29 17:43:39 +0200

    New subscription activity linked to payments activity

--- app/src/main/java/edu/cmu/mutuelle/mutuelle/NewSubscriptionActivity.java
+++ app/src/main/java/edu/cmu/mutuelle/mutuelle/NewSubscriptionActivity.java
@@ -1,13 +1,32 @@
 package edu.cmu.mutuelle.mutuelle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class NewSubscriptionActivity extends AppCompatActivity {
+    private Spinner insurancePlanSpinner;
+    private Button subscribeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_subscription);
+
+        //initiate the views
+        insurancePlanSpinner = (Spinner) findViewById(R.id.subscriptionPlanList);
+        subscribeButton = (Button) findViewById(R.id.subscribeButton);
+        subscribeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent payments = new Intent(NewSubscriptionActivity.this, PaymentActivity.class);
+                //Transfer the selected insurance plan
+                //String selectedPlan = insurancePlanSpinner.getSelectedItem().toString();
+                startActivity(payments);
+            }
+        });
     }
 }
