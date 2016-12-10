e6a0646025211735c7e1060a2b19e6a27038d0fd
romalice1 <rommy_91@hotmail.fr>
2016-12-10 18:42:12 +0200

    added customer image on checkin

--- app/src/main/java/edu/cmu/mutuelle/mutuelle/CheckInActivity.java
+++ app/src/main/java/edu/cmu/mutuelle/mutuelle/CheckInActivity.java
@@ -1,8 +1,12 @@
 package edu.cmu.mutuelle.mutuelle;
 
+import android.app.FragmentTransaction;
 import android.graphics.Bitmap;
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.google.zxing.BarcodeFormat;
@@ -12,10 +16,12 @@ import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import edu.cmu.mutuelle.mutuelle.R;
+import edu.cmu.mutuelle.mutuelle.fragment.ViewCustomerImageFragment;
 
 public class CheckInActivity extends AppCompatActivity {
 
     ImageView QRCodeImage;
+    ImageButton viewCustomerImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,20 @@ public class CheckInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_check_in);
 
         QRCodeImage = (ImageView) findViewById(R.id.QRCodeImage);
+        viewCustomerImage = (ImageButton) findViewById(R.id.customerImageView);
+
+        /*
+        viewCustomerImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ViewCustomerImageFragment customerImageFragment = new ViewCustomerImageFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.add(R.id.ImageFragmentContainer, customerImageFragment);
+                transaction.commit();
+            }
+        });
+        */
 
         MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
         String textToEncode = "Text to convert";
