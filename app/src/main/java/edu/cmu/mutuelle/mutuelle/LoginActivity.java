c19ce8a42bcf18c2f365d4ce1b213bd40a471e9e
amuhebwa <muhebwa.grey@gmail.com>
2016-12-10 19:28:06 +0200

    Added Camera Activity

--- app/src/main/java/edu/cmu/mutuelle/mutuelle/LoginActivity.java
+++ app/src/main/java/edu/cmu/mutuelle/mutuelle/LoginActivity.java
@@ -5,19 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText usernameTxt, passwordTxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+        usernameTxt = (EditText) findViewById(R.id.username);
+        passwordTxt = (EditText) findViewById(R.id.password);
+
         Button signUpButton = (Button) findViewById(R.id.signupButton);
         Button loginButton = (Button) findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, OverViewActivity.class));
+                validateUserDetails();
             }
         });
         signUpButton.setOnClickListener(new View.OnClickListener() {
@@ -26,5 +30,13 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
             }
         });
+    }
+    // validate user details
+    private void validateUserDetails(){
+        String username = usernameTxt.getText().toString();
+        String password = passwordTxt.getText().toString();
+
+        // check if user exists in the database. if true, login, else show a toast message
+        startActivity(new Intent(LoginActivity.this, OverViewActivity.class));
     }
 }
