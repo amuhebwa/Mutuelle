bd3190ea2999827042b4d6446d8890ff8638cb78
romalice1 <rommy_91@hotmail.fr>
2016-12-11 22:11:50 +0200

    QR code scanner fixed :)

--- app/src/main/java/edu/cmu/mutuelle/mutuelle/hospital/HospitalCheckinPatientActivity.java
+++ app/src/main/java/edu/cmu/mutuelle/mutuelle/hospital/HospitalCheckinPatientActivity.java
@@ -1,5 +1,6 @@
 package edu.cmu.mutuelle.mutuelle.hospital;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,7 +27,13 @@ public class HospitalCheckinPatientActivity extends AppCompatActivity implements
         scanButton = (Button) findViewById(R.id.scannerButton);
         scanResultView = (TextView) findViewById(R.id.scanResultView);
 
-
+        //If there is an intent from the QR result
+        Intent intent = getIntent();
+        if(intent != null)
+        {
+            String scanResult = intent.getStringExtra("scanResult");
+            scanResultView.setText(scanResult);
+        }
 
         //Start scanning
         scanButton.setOnClickListener(new View.OnClickListener() {
@@ -50,10 +57,12 @@ public class HospitalCheckinPatientActivity extends AppCompatActivity implements
     public void handleResult(Result result) {
         Log.d("qrcode","Result available");
         Log.d("qrcode","camera stopped");
-        scanResultView.setText(result.getText());
+        //scanResultView.setText(result.getText());
         Log.d("qrcode","text set");
-    }
 
-    
+        Intent intent = new Intent(HospitalCheckinPatientActivity.this, HospitalCheckinPatientActivity.class);
+        intent.putExtra("scanResult", result.getText());
+        startActivity(intent);
+    }
 }
 
