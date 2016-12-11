dcf4498d16e6c0f3557b50e8ce2b4ee62f18fc71
amuhebwa <muhebwa.grey@gmail.com>
2016-12-11 12:21:27 +0200

    added iternationalization

--- app/src/main/java/edu/cmu/mutuelle/mutuelle/NearbyActivity.java
+++ app/src/main/java/edu/cmu/mutuelle/mutuelle/NearbyActivity.java
@@ -1,14 +1,119 @@
 package edu.cmu.mutuelle.mutuelle;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 
-public class NearbyActivity extends FragmentActivity{
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class NearbyActivity extends FragmentActivity
+        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        LocationListener, OnMapReadyCallback {
+    private GoogleApiClient googleApiClient;
+    private GoogleMap googleMap;
+    private static final int REQUEST_CODE_LOCATION = 2;
+    private double latitude = 0.000;
+    private double longitude = 0.000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setUpGoogleCLientAPI();
         setContentView(R.layout.activity_nearby);
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().
+                findFragmentById(R.id.googleMaps);
+        supportMapFragment.getMapAsync(this);
+
     }
 
+    private void setUpGoogleCLientAPI() {
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_LOCATION);
+        } else {
+            googleMap.setMyLocationEnabled(true);
+            LocationRequest locationRequest = LocationRequest.create();
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            locationRequest.setInterval(10000);
+            locationRequest.setFastestInterval(5000);
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+
+            Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            latitude = mLastLocation.getLatitude();
+            longitude = mLastLocation.getLongitude();
+            showPatientsCurrentLocation(latitude, longitude);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (googleApiClient.isConnected()) {
+            googleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    private void showPatientsCurrentLocation(double latitude, double longitude) {
+        LatLng currentLocation = new LatLng(latitude, longitude);
+        googleMap.addMarker(new MarkerOptions().position(currentLocation).title("You Are Here"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+    }
 }
